41692fdba3e1ed75ec24c2a766e6a8d9f88f0a96
Ryan <ryansaftje123@gmail.com>
2024-06-02 15:10:17 +0700

    Update backend

--- CrystalRestaurant_BE/_controller/dine_inController.js
+++ CrystalRestaurant_BE/_controller/dine_inController.js
@@ -1,23 +1,22 @@
 const pool = require("./_pool");
 const crypto = require('crypto');
-
+const moment = require('moment-timezone');
 
 function md5Hash(text) {
     return crypto.createHash('md5').update(text).digest('hex');
 }
 
 createSession = async(req, res) => {
-    const now = new Date();
-    const formattedDate = now.toISOString();
-
-    const hashedSession = md5Hash(now + formattedDate);
+    const Datenow = new Date();
+    const indonesiaTime = moment(Datenow).tz('Asia/Jakarta').format();
+    const hashedSession = md5Hash(Datenow + indonesiaTime);
 
     const qr_code = "http://localhost:5173/" + hashedSession;
     try{
         const result = await pool.query(
             `INSERT INTO Dine_in(qr_code, dine_time)
             VALUES($1, $2) RETURNING *`,
-            [qr_code, formattedDate]
+            [qr_code, indonesiaTime]
         );
         res.status(200).json(result.rows[0]);
     }catch(err){
