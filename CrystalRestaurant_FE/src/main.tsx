890ea03db3f348a427affb34682e920625a9bbe0
EdgrantHS <edgrant.h@gmail.com>
2024-06-05 17:38:48 +0700

    dynamic routing and sessions

--- CrystalRestaurant_FE/src/main.tsx
+++ CrystalRestaurant_FE/src/main.tsx
@@ -1,12 +1,12 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import ReactDOM from "react-dom/client"
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import "./index.css"
 
 import Navbar from "./components/Navbar.tsx"
 
 import MenuPage from "./pages/MenuPage/MenuPage.tsx"
-import ColorPalette from "./pages/ColorPalette.tsx"
+// import ColorPalette from "./pages/ColorPalette.tsx"
 import ErrorPage from "./pages/ErrorPage.tsx"
 import LoginPage from "./pages/LoginPage.tsx"
 import OrderPage from "./pages/OrderPage.tsx"
@@ -14,8 +14,8 @@ import RegisterPage from "./pages/RegisterPage.tsx"
 import ProfilePage from "./pages/ProfilePage.tsx"
 import AdminPage from "./pages/AdminPage.tsx"
 
-import { atom, /*createStore,*/ Provider } from "jotai"
-
+import { atom, Provider, useAtom } from "jotai"
+import { getSession } from "./actions/Dine.actions.ts"
 interface Order {
   id: number
   title: string
@@ -26,19 +26,39 @@ interface Order {
 export const orderAtom = atom<Order[]>([])
 export const usernameAtom = atom<string | null>(null)
 
-// const store = createStore()
-
 function App() {
+  const [sessions, setSessions] = useState<string[]>([])
+
+  useEffect(() => {
+    getSession().then((data) => {
+      setSessions(data)
+    })
+  }, [])
+
+  console.log(sessions)
+
   return (
     <React.StrictMode>
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Navigate to='/menu' />} />
-          <Route path='/menu' element={<MenuPage />} />
-          <Route path='/order' element={<OrderPage />} />
+          {sessions.map((session) => (
+            <Route
+              key={session}
+              path={`/${session}/menu`}
+              element={<MenuPage />}
+            />
+          ))}
+          {sessions.map((session) => (
+            <Route
+              key={session}
+              path={`/${session}/order`}
+              element={<OrderPage />}
+            />
+          ))}
+          <Route path='/' element={<h1>hellowold</h1>} />
           <Route path='*' element={<ErrorPage />} />
-          <Route path='/color-palette' element={<ColorPalette />} />
+          {/* <Route path='/color-palette' element={<ColorPalette />} /> */}
           <Route path='/login' element={<LoginPage />} />
           <Route path='/register' element={<RegisterPage />} />
           <Route path='/profile' element={<ProfilePage />} />
