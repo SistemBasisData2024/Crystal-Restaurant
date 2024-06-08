53408eb99192662c46c05f79142b1e99e8172731
EdgrantHS <edgrant.h@gmail.com>
2024-06-09 00:46:15 +0700

    keperluan merge

--- CrystalRestaurant_FE/src/main.tsx
+++ CrystalRestaurant_FE/src/main.tsx
@@ -1,19 +1,18 @@
 import React, { useEffect, useState } from "react"
 import ReactDOM from "react-dom/client"
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import "./index.css"
 
 import Navbar from "./components/Navbar.tsx"
 
 import MenuPage from "./pages/MenuPage/MenuPage.tsx"
-// import ColorPalette from "./pages/ColorPalette.tsx"
 import ErrorPage from "./pages/ErrorPage.tsx"
 import OrderPage from "./pages/OrderPage.tsx"
-import ProfilePage from "./pages/LoginRegister/ProfilePage.tsx"
 import AdminPage from "./pages/AdminPage.tsx"
 
 import { atom, Provider } from "jotai"
-import { dineIn, getSession } from "./actions/Dine.actions.ts"
+import { getSession } from "./actions/Dine.actions.ts"
+import InputSessionPage from "./pages/InputSessionPage.tsx"
 interface Order {
   id: number
   title: string
@@ -56,30 +55,9 @@ function App() {
               element={<OrderPage />}
             />
           ))}
-          {/* {sessions.map((session) => (
-            <Route
-              key={session}
-              path={`/${session}/login`}
-              element={<LoginPage />}
-            />
-          ))}
-          {sessions.map((session) => (
-            <Route
-              key={session}
-              path={`/${session}/register`}
-              element={<RegisterPage />}
-            />
-          ))} */}
-          <Route path='/' element={<Navigate to="/login" />} />
+          <Route path='/' element={<InputSessionPage />} />
           <Route path='*' element={<ErrorPage />} />
-          {/* <Route path='/color-palette' element={<ColorPalette />} /> */}
-          {sessions.map((session) => (
-            <Route key={session} path={`/${session}/profile`} element={<ProfilePage />} />
-          ))}
-          {/* <Route path='/profile/*' element={<ProfilePage />} /> */}
           <Route path='/admin' element={<AdminPage />} />
-          {/* <Route path='/login/*' element={<LoginPage />} />
-          <Route path='/register/*' element={<RegisterPage />} /> */}
         </Routes>
       </Router>
     </React.StrictMode>
---
+++ CrystalRestaurant_FE/src/pages/InputSessionPage.tsx
@@ -0,0 +1,7 @@
+export default function InputSessionPage() {
+  return (
+    <section className=' absolute left-0 top-0 -z-10 h-screen w-screen flex-col bg-gradient-to-t from-bgdull-100 to-bgdull-200' >
+      d
+    </section>
+  )
+}
