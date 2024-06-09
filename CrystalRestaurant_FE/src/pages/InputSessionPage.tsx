05301134e3e4723dd534356bdf31775f57c39db7
Ryan <ryansaftje123@gmail.com>
2024-06-09 22:55:25 +0700

    Menambahkan Landing page

--- CrystalRestaurant_FE/src/pages/InputSessionPage.tsx
+++ CrystalRestaurant_FE/src/pages/InputSessionPage.tsx
@@ -4,11 +4,12 @@ export default function InputSessionPage() {
       <section className="bg-gradient-to-r from-blue-900 to-blue-800 text-white h-screen flex items-center">
         <div className="container mx-auto text-center px-6">
           <h1 className="text-4xl md:text-6xl font-bold mb-4">Welcome to Crystal Restaurant</h1>
-          <p className="text-lg md:text-2xl mb-8">Digitalized your menu.</p>
+          <p className="text-lg md:text-2xl mb-5">Digitalized your menu.</p>
+          
           <button
             className="bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-6 rounded-full"
           >
-            Get Started
+            Contact our Waiter to get your table!
           </button>
         </div>
       </section>
