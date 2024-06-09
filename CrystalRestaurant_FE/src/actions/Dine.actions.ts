b4550cf9f666ed62c5376884166a2561af78e539
Ryan <ryansaftje123@gmail.com>
2024-06-09 17:48:54 +0700

    Membuat delete session berfungsi

--- CrystalRestaurant_FE/src/actions/Dine.actions.ts
+++ CrystalRestaurant_FE/src/actions/Dine.actions.ts
@@ -1,46 +1,56 @@
-import axios from "axios"
+import axios from "axios";
 
 // formData => req.body (used for param in endpoint call)
 
-const baseApiResponse = (data: any, isSuccess: boolean) => {
-  return {
-    success: isSuccess,
-    data: data || null,
-  }
-}
+const baseApiResponse = (
+    data: any, isSuccess: boolean) => {
+    return {
+        success: isSuccess,
+        data: data || null,
+    };
+};
 
 export const dineIn = async () => {
-  try {
-    const response = await axios.get(
-      `${import.meta.env.VITE_API_URL}/dine/create`
-    )
-    console.log("RESPONSE FROM BACKEND")
-    console.log(response.data)
-    return baseApiResponse(response.data, true)
-  } catch (error: any) {
-    console.error(error)
-    return baseApiResponse(null, false)
-  }
-}
+    try {
+        const response = await axios.get(
+            `${import.meta.env.VITE_API_URL}/dine/create`
+        );
+        console.log("RESPONSE FROM BACKEND");
+        console.log(response.data);
+        return baseApiResponse(response.data, true);
+    } catch (error: any) {
+        console.error(error);
+        return baseApiResponse(null, false);
+    }
+};
 
 export const getSession = async () => {
-  try {
-    const response = await axios.get(
-      `${import.meta.env.VITE_API_URL}/dine/getall`
-    )
-    console.log("RESPONSE FROM BACKEND")
-    console.log(response.data)
-    //return baseApiResponse(["qwjbhkefhbjqwfebiqwuyaef", "ad7a28e74acc141b498d1c53768f5b6c"], true);
-    const sessions = Array.isArray(response.data)
-      ? response.data.map((item) => item.session)
-      : response.data
-    return baseApiResponse(sessions, true)
-  } catch (error: any) {
-    console.error(error)
-    return baseApiResponse(null, false)
-  }
+    try {
+        const response = await axios.get(
+            `${import.meta.env.VITE_API_URL}/dine/getall`
+        );
+        console.log("RESPONSE FROM BACKEND");
+        console.log(response.data);
+        const sessions = Array.isArray(response.data) ? response.data.map(item => item.session) : response.data;
+        return baseApiResponse(sessions, true);
+
+    } catch (error: any) {
+        console.error(error);
+        return baseApiResponse(null, false);
+    }
 }
 
-export const deleteSession = async (sessionId: string) => {
-  return baseApiResponse(sessionId, true)
+export const deleteSession = async (session : string) => {
+    try {
+        const response = await axios.delete(
+            `${import.meta.env.VITE_API_URL}/dine/delete/${session}`
+        );
+        console.log("RESPONSE FROM BACKEND");
+        console.log(response.data);
+        return baseApiResponse(response.data, true);
+
+    } catch (error: any) {
+        console.error(error);
+        return baseApiResponse(null, false);
+    }
 }
